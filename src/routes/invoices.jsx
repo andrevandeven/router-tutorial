da7d790e17ae51694c698d33a2c9471c55bfe6e5
andrevandeven <andrev@sas.upenn.edu>
2022-11-05 13:43:25 -0400

    third commit

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,7 +1,23 @@
+import { Link, Outlet} from "react-router-dom";
+import { getInvoices } from "../data";
+ 
 export default function Invoices() {
-    return (
-        <div style={{ padding: "1rem 0" }}>
-            <h2>Invoices</h2>
-        </div>
-    );
+  let invoices = getInvoices(); {/* import invoice data */}
+  return (
+    <div style={{ display: "flex" }}>
+      <nav>
+        {/* iterate through all invoices */}
+            {invoices.map((invoice) => (
+            <Link
+                style={{ display: "block", margin: "1rem 0" }}
+                to={`/invoices/${invoice.number}`} 
+                key={invoice.number}
+            >
+                {invoice.name}
+            </Link>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
